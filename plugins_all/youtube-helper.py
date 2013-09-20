6400ac8f75df70fc9ecd948e7ae525c9d15204ac
Helmut Irle <helmut@devigninnovation.com>
2013-09-20 17:32:55 +0200

    Refactored and catered for shortened YT links.

--- plugins_all/youtube-helper.py
+++ plugins_all/youtube-helper.py
@@ -1,6 +1,6 @@
 from message_plugin import MessagePlugin
 
-from urllib2 import urlopen
+from urllib2 import urlopen, HTTPError
 from urlparse import urlparse, parse_qs
 import datetime
 import re
@@ -9,31 +9,49 @@ import xml.etree.ElementTree as ET
 
 class YoutubeHelper(MessagePlugin):
 
+    def get_video_id(self, url):
+        if url.find("youtube.com") > -1:
+            qps = parse_qs(urlparse(url).query, keep_blank_values=True)
+            if 'v' in qps.keys():
+                return qps['v'][0]
+            else:
+                return None
+        elif url.find("youtu.be") > -1:
+            path_elements = filter(None, urlparse(url).path.split("/"))
+            if len(path_elements) > 0:
+                return path_elements[0]
+            else:
+                return None
+        else:
+            return None
+
+    def get_video_info(self, video_id):
+        try:
+            fh = urlopen("http://gdata.youtube.com/feeds/api/videos/%s" % (video_id))
+            xml_data = fh.read()
+            root = ET.fromstring(xml_data)
+            video_title = ""
+            video_duration = ""
+            for k in range(0, len(root) - 1):
+                if root[k].tag.endswith("title"):
+                    video_title = root[k].text
+                if root[k].tag.endswith("group"):
+                    for child in root[k].getchildren():
+                        if child.tag.endswith("duration"):
+                            video_duration = child.attrib['seconds']
+            fh.close()
+            return video_title, video_duration
+        except HTTPError:
+            return None, None
+
     def message_received(self, msg, nick=""):
         if msg['type'] == 'groupchat':
-            # Get urls in message
             urls = re.findall(r'(https?://[^\s]+)', msg['body'])
 
-            new_msg = ""
             for url in urls:
-                # Check if link is from Youtube
-                if url.find("youtube.com") > -1:
-                    # Find video ID from query parameters
-                    qps = parse_qs(urlparse(url).query, keep_blank_values=True)
-                    if 'v' in qps.keys():
-                        # Get video details
-                        fh = urlopen("http://gdata.youtube.com/feeds/api/videos/%s" % (qps['v'][0]))
-                        xml_data = fh.read()
-                        root = ET.fromstring(xml_data)
-                        video_title = ""
-                        video_duration = ""
-                        for k in range(0, len(root) - 1):
-                            if root[k].tag.endswith("title"):
-                                video_title = root[k].text
-                            if root[k].tag.endswith("group"):
-                                for child in root[k].getchildren():
-                                    if child.tag.endswith("duration"):
-                                        video_duration = child.attrib['seconds']
-                        new_msg += "Video: %s (%s)" % (video_title, str(datetime.timedelta(seconds=int(video_duration))))
-                        fh.close()
-            msg.reply(new_msg).send()
+                video_id = self.get_video_id(url)
+                if video_id:
+                    title, duration = self.get_video_info(video_id)
+                    if title:
+                        new_msg = "Video: %s (%s)" % (title, str(datetime.timedelta(seconds=int(duration))))
+                        msg.reply(new_msg).send()
